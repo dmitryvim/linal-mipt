9551031890df91b5e42503d11f6bd3e0523826cd
Dmitry Mikhaylovich <dmitry.mikhailovich@gmail.com>
2017-03-28 09:25:57 +0300

    fix start matrix

--- src/matrix/Matrix.h
+++ src/matrix/Matrix.h
@@ -12,6 +12,9 @@ private:
     int row_count;
     int col_count;
     T** values;
+
+    void set_sizes(const int row_count, const int col_count);
+    void free();
 public:
     Matrix(const int row_count, const int col_count);
     Matrix(const Matrix &that);
@@ -19,6 +22,60 @@ public:
 
     void print();
 };
+
+template <typename T>
+Matrix<T>::Matrix(const int row_count, const int col_count) {
+    set_sizes(row_count, col_count);
+    for (int row = 0; row < row_count; ++row) {
+        for (int col = 0; col < col_count; ++col) {
+            this->values[row][col] = 0;
+        }
+    }
+}
+
+template <typename T>
+void Matrix<T>::set_sizes(const int row_count, const int col_count) {
+    this->row_count = row_count;
+    this->col_count = col_count;
+    this->values = new T*[row_count];
+    for (int row = 0; row < row_count; ++row) {
+        this->values[row] = new T[col_count];
+    }
+}
+
+template <typename T>
+Matrix<T>::Matrix(const Matrix &that) {
+    set_sizes(that.row_count, that.col_count);
+    for (int row = 0; row < row_count; ++row) {
+        for (int col = 0; col < col_count; ++col) {
+            this->values[row][col] = that.values[row][col];
+        }
+    }
+}
+
+template <typename T>
+Matrix<T>::~Matrix() {
+    free();
+}
+
+template <typename T>
+void Matrix<T>::free() {
+    for (int row = 0; row < row_count; ++row)
+    {
+        delete []this->values[row];
+    }
+    delete []this->values;
+}
+
+template <typename T>
+void Matrix<T>::print() {
+    for (int row = 0; row < row_count; ++row) {
+        for (int col = 0; col < col_count; ++col) {
+            std::cout << this->values[row][col] << " ";
+        }
+        std::cout << std::endl;
+    }
+}
 
 
 #endif //LINAL_MIPT_MATRIX_H
--- src/matrix/test.cpp
+++ src/matrix/test.cpp
@@ -5,17 +5,27 @@
 #include <iostream>
 #include "Matrix.h"
 
+int __test_counter = 0;
+
 void assert(const bool statement, const char *message) {
     if (!statement) {
-        std::cerr << "Test error: " << message;
+        std::cerr << "Test error: " << message << std::endl;
     }
+}
+
+void next_test(const char * test_name)
+{
+    std::cout << std::endl << "Test " << ++__test_counter << ": " << test_name << std::endl;
 }
 
 void test_runnable() {
     Matrix<int> matrix(2, 2);
-    assert(true, "unnable to create matrix");
+    matrix.print();
+    assert(true, "Hope, zero 2x2 matrix was printed");
 }
 
 int main() {
+
+    next_test("print zero 2x2 matrix");
     test_runnable();
 }
